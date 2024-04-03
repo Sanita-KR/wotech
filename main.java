9d16516235ba75e38e6cd2781db8d0ec5af001ec
Sanita-KR <98774045+Sanita-KR@users.noreply.github.com>
2024-04-03 19:19:28 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,23 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+    String season = "spring";
+
+    if (season == "winter") {
+      System.out.println("You should wear a warm jacket.");
+    }
+    else {
+      System.out.println("It is spring and you should wear a light jacket.");
+    }
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
